c8d25e1304a22500ca803d05bbc275cf27810fa0
Max Boldyrev <bm456@ya.ru>
2025-11-13 20:46:31 +0300

    Removed dulpex io. Need testing

--- utils/audio.py
+++ utils/audio.py
@@ -125,61 +125,79 @@ class AudioIO(Thread):
     def __init__(
         self,
         length: float,
-        device: tuple[int, int] | None = None,
+        device: tuple[int, int] | tuple[None, None] = (None, None),
         gen_mode: GenMode = GenMode.LOG_SWEEP,
         band: tuple[float, float] = (20, 20000),
         ref: RefMode = RefMode.CHANNEL_B,
     ) -> None:
-        self.length: float = length
-        self.device: tuple[int, int] | None = device
-        self.gen_mode: GenMode = gen_mode
-        self.band: tuple[float, float] = band
-        self.fs: int = 0
-        self.n: int = 0
-        self.record: np.ndarray = np.empty((0, 2), np.float32)
-        self.signal: np.ndarray = np.empty((0, 2), np.float32)
-        self.ref: RefMode = ref
-        self.position: int = 0
+        self.length = length
+        self.device: tuple[int | None, int | None] = device
+        self.gen_mode = gen_mode
+        self.band = band
+        self.in_fs = 0
+        self.in_n = 0
+        self.out_fs = 0
+        self.out_n = 0
+        self.record = np.empty((0, 2), np.float32)
+        self.signal = np.empty((0, 2), np.float32)
+        self.ref = ref
+        self.out_position = 0
+        self.in_position = 0
         self.record_lock = Lock()
         self.running = Event()
         self.record_updated = Event()
         self.levels_updated = Event()
         self.exit = Event()
-        self.stop = Event()
-        self.cb_ts = np.zeros(1000, int)
-        self.cb_ns = np.zeros(1000, int)
-        self.cb_i = 0
-        self.cb_t0 = 0.0
+        self.out_stop = Event()
+        self.in_stop = Event()
+        self.padding_time = 0.2
 
-        self.start_time: float = 0
+        self.start_time = 0
 
         return super().__init__(daemon=True)
 
-    def run(self):
+    def run(self) -> None:
         if WIN32:
             pythoncom.CoInitializeEx(0)  # type: ignore
         try:
             while not self.exit.is_set():
                 self.running.wait()
-                self.stop.clear()
+                self.in_stop.clear()
+                self.out_stop.clear()
                 if not self.exit.is_set():
-                    stream = sd.Stream(device=self.device, callback=self.callback)
+                    in_stream = sd.InputStream(
+                        device=self.device[0],
+                        callback=self.input_callback,
+                    )
+                    self.in_fs = in_stream.samplerate
+                    self.in_n = int((self.length + self.padding_time) * self.in_fs)
 
-                    self.fs = stream.samplerate
-                    self.n = int(self.length * self.fs) if self.fs else 0
+                    self.record = np.zeros((self.in_n, 2), np.float32)
+                    self.in_position = 0
 
-                    self.record = np.zeros((self.n, 2), np.float32)
-                    self.position = 0
-
+                    out_stream = sd.OutputStream(
+                        device=self.device[1],
+                        callback=self.output_callback,
+                    )
+                    self.out_fs = out_stream.samplerate
+                    self.out_n = int(self.length * self.out_fs)
+                    self.out_position = 0
                     if self.gen_mode == GenMode.LOG_SWEEP:
-                        self.signal = log_sweep(self.n, self.fs, self.band)
+                        self.signal = log_sweep(self.out_n, self.out_fs, self.band)
                     if self.gen_mode == GenMode.PINK_NOISE:
-                        self.signal = pink_noise(self.n, self.fs, self.band)
+                        self.signal = pink_noise(self.out_n, self.out_fs, self.band)
+                    out_padding = int(self.out_fs * self.padding_time)
+                    self.signal = np.pad(self.signal, ((out_padding, 0), (0, 0)))
+                    self.out_n += out_padding
 
-                    self.first_cb = True
-                    stream.start()
-                    self.stop.wait()
-                    stream.close()
+                    in_stream.start()
+                    out_stream.start()
+                    self.out_stop.wait()
+                    self.in_stop.wait()
+                    in_stream.stop()
+                    out_stream.stop()
+                    in_stream.close()
+                    out_stream.close()
                 self.running.clear()
         except Exception as e:
             print(f"AudioIO exception: {e}")
@@ -188,60 +206,62 @@ class AudioIO(Thread):
                 pythoncom.CoUninitialize()  # type: ignore
             self.running.clear()
 
-    def callback(
+    def output_callback(
         self,
-        indata: np.ndarray,
         outdata: np.ndarray,
         n: int,
         time: PaStreamCallbackTimeInfo,
         status: sd.CallbackFlags,
     ) -> None:
-
-        if self.cb_i < 1000:
-            self.cb_ts[self.cb_i] = monotonic_ns()
-            self.cb_ns[self.cb_i] = n
-            self.cb_i += 1
-
-        if not self.cb_t0:
-            self.cb_t0 = time.currentTime
-
-        if status.output_underflow:
-            print("Output underflow:", status, time.currentTime - self.cb_t0)
-
-        if status.input_overflow:
-            print("Input overflow:", status, time.currentTime - self.cb_t0)
-
-        s = self.position
-        e = min(s + n, self.n)
-        # outdata[:] = np.zeros((n, 2), np.float32)
+        s = self.out_position
+        e = min(s + n, self.out_n)
+        outdata.fill(0)
         outdata[: e - s, :2] = self.signal[s:e]
-        # outdata[:, :2] = self.signal[s:s+n]
+        self.out_position = e
 
-        # with self.record_lock:
-        # self.record[s:e] = indata[: e - s, :2]
-        self.position = e
-        # self.position += n
+        if e >= self.out_n:
+            self.out_stop.set()
 
-        # self.record_updated.set()
-        # self.levels_updated.set()
-
-        # if self.position >= self.n or not self.running.is_set():
-        if self.position >= self.n:
-            self.stop.set()
-
-    def get_record(self):
+    def input_callback(
+        self,
+        indata: np.ndarray,
+        n: int,
+        time: PaStreamCallbackTimeInfo,
+        status: sd.CallbackFlags,
+    ) -> None:
         with self.record_lock:
-            data = self.record[: self.position].copy()
+            s = self.in_position
+            e = min(s + n, self.in_n)
+            if indata.shape[1] < 2:
+                indata = np.repeat(indata, 2, axis=1)
+            self.record[s:e] = indata[: e - s, :2]
+            self.in_position = e
+
+        self.levels_updated.set()
+        self.record_updated.set()
+
+        if e >= self.in_n:
+            self.in_stop.set()
+
+    def get_record(self) -> np.ndarray:
+        with self.record_lock:
+            data = self.record[: self.in_position].copy()
         self.record_updated.clear()
         if self.ref == RefMode.GENERATOR:
             data[:, 1] = self.signal[: len(data), 0]
         return data
 
-    def get_levels(self, time_step: float = 0.1):
+    def get_levels(self, time_step: float = 0.1) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Returns:
+            ts (np.ndarray): 1D array of time stamps (shape: (N,)), where N is the number of chunks.
+            levels (np.ndarray): 2D array of shape (2, N), where levels[0] is the max level for channel 1,
+                                 and levels[1] is the max level for channel 2, for each time chunk.
+        """
         with self.record_lock:
-            e = self.position
+            e = self.in_position
             data = self.record[:e].copy()
-        chunk_size = int(time_step * self.fs)
+        chunk_size = int(time_step * self.in_fs)
         ss = np.arange(0, e, chunk_size)
         es = ss + chunk_size
         ts = np.arange(len(ss)) * time_step
@@ -255,15 +275,18 @@ class AudioIO(Thread):
     def kill(self):
         self.exit.set()
         self.running.set()
-        self.stop.set()
+        self.in_stop.set()
+        self.out_stop.set()
 
     def run_once(self):
         self.running.set()
-        self.stop.wait()
+        self.in_stop.wait()
+        self.out_stop.wait()
 
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    from matplotlib.axes import Axes
     from scipy.signal import welch, periodogram
     from scipy.signal.windows import gaussian
 
@@ -282,21 +305,33 @@ if __name__ == "__main__":
         print(dev)
 
     # stream = sd.Stream()
-    io = AudioIO(10, gen_mode=GenMode.LOG_SWEEP, band=(100, 20000))
+    io = AudioIO(10, gen_mode=GenMode.LOG_SWEEP, band=(100, 20000), device=(14, 12))
     io.start()
     io.run_once()
-    # ts, levels = io.get_levels(0.1)
-    # # plt.plot(ts, 20*np.log10(levels[0].clip(1e-12)), label="Channel 1")
-    # # plt.plot(ts, 20*np.log10(levels[1].clip(1e-12)), label="Channel 2")
+    # while True:
+    #     io.run_once()
+    figure, plots = plt.subplots(2, 1, figsize=(8, 6))
+    plots: list[Axes]
+    ts, levels = io.get_levels(0.1)
+    plots[0].plot(ts, 20 * np.log10(levels[0].clip(1e-12)), label="Channel 1")
+    plots[0].plot(ts, 20 * np.log10(levels[1].clip(1e-12)), label="Channel 2")
 
+    rec = io.get_record()
+    fs, Pxx = periodogram(rec, fs=io.in_fs, axis=0)
+    plots[1].semilogx(
+        fs, 10 * np.log10((fs * Pxx[:, 0]).clip(1e-20)), label="Channel 1"
+    )
+    plots[1].semilogx(
+        fs, 10 * np.log10((fs * Pxx[:, 1]).clip(1e-20)), label="Channel 2"
+    )
     # a, b = io.get_record().T
     # ts = np.arange(len(a)) / io.fs
     # plt.plot(ts, a, label="Channel 1")
     # plt.plot(ts, b, label="Channel 2")
-    ts = np.diff(io.cb_ts[: io.cb_i])
-    ns = np.cumsum(io.cb_ns[: len(ts)]) / io.fs * 1e9 - np.cumsum(ts)
-    plt.plot(np.cumsum(ts) / 1e9, ts)
-    plt.plot(np.cumsum(ts) / 1e9, ns)
+    # ts = np.diff(io.cb_ts[: io.cb_i])
+    # ns = np.cumsum(io.cb_ns[: len(ts)]) / io.fs * 1e9 - np.cumsum(ts)
+    # plt.plot(np.cumsum(ts) / 1e9, ts)
+    # plt.plot(np.cumsum(ts) / 1e9, ns)
 
     # # plt.plot(x)
     # # print()
@@ -311,5 +346,7 @@ if __name__ == "__main__":
     # plt.semilogx(log_f, 10 * np.log10((log_f * log_p).clip(1e-20)))
 
     # # plt.plot(10 * np.log10(w))
-    # plt.grid(True, "both")
+    plt.legend()
+    plots[0].grid(True, "both")
+    plots[1].grid(True, "both")
     plt.show()
