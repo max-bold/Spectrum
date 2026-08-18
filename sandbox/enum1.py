dfbe724c2b5c2556ca47eaa88fea900079264c0a
Max Boldyrev <bm456@ya.ru>
2026-08-18 04:09:49 +0300

    Prepare clean v0.3 release tree

--- sandbox/enum1.py
+++
@@ -1,3 +0,0 @@
-from typing import Literal
-
-def printenum(a:enumerate[])
--- sandbox/pink_noise.py
+++
@@ -1,6 +0,0 @@
-from utils.generators import pink_noise
-import sounddevice as sd
-
-stream = sd.OutputStream(channels=1)
-
-wf = pink_noise(30,stream.device.)
