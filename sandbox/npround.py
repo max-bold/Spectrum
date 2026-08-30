39468d0f3d1d655f16c08b1993208600bc851a90
Max Boldyrev <bm456@ya.ru>
2026-08-31 00:15:10 +0300

    Prepare v0.3.1 release

--- sandbox/npround.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-a = np.array((0.75,0.125,1.125,0.997))
-print(np.rint(a))
-print(np.astype(a,int))
-print(type(np.rint(a)[0]))
--- spectrum_app_old/ui/__init__.py
+++
@@ -1,8 +0,0 @@
-from .refs import ControlRefs, FileDialogs, PlotRefs, UiRefs
-
-__all__ = [
-    "ControlRefs",
-    "FileDialogs",
-    "PlotRefs",
-    "UiRefs",
-]
